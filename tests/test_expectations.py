f455742e143ede4a57fdd95f0bd9976b66f28188
Matheus Brizolla <matheusfnx@hotmail.com>
2025-01-27 21:47:49 +0000

    feat: add new unit tests

--- tests/test_expectations.py
+++ tests/test_expectations.py
@@ -1,9 +1,24 @@
-import json
 import pytest
 import pandas as pd
 
-from unittest.mock import patch
+from enum import Enum
+from typing import Dict
+from unittest.mock import patch, MagicMock
 from src.great_expectations.expectations import GreatExpectationsChecker
+
+
+class MockConfig(Enum):
+    CONTEXT_MODE: str = "mock_mode"
+    DATA_SOURCE: str = "mock_source"
+    DATA_ASSET: str = "mock_data_asset"
+    SITE_NAME: str = "mock_site"
+    SITE_CONFIG: Dict[str, str] = {"mock_key": "mock_value"}
+    SUITE_NAME = "mock_suite"
+
+
+@pytest.fixture
+def mock_config():
+    return MockConfig
 
 
 @pytest.fixture
@@ -53,18 +68,88 @@ def mock_df():
     return pd.DataFrame(data)
 
 
-@patch("src.great_expectations.expectations.gx.get_context")
-def test_initilization(mock_get_context, mock_df):
-    context_mode = "mock_mode"
+@pytest.fixture
+def mock_get_context():
+    with patch("src.great_expectations.expectations.gx.get_context") as mock:
+        mock_context = MagicMock()
+        mock.return_value = mock_context
+        yield mock
+
+
+# @patch("src.great_expectations.expectations.gx.get_context")
+def test_initilization(mock_get_context, mock_df, mock_config):
     mock_context = mock_get_context.return_value
 
-    result = GreatExpectationsChecker(mock_df, context_mode)
+    result = GreatExpectationsChecker(mock_df, mock_config.CONTEXT_MODE)
 
     assert isinstance(result.df, pd.DataFrame)
     assert len(result.df) == 2
     assert result.context == mock_context
-    mock_get_context.assert_called_once_with(mode=context_mode) 
+    mock_get_context.assert_called_once_with(mode=mock_config.CONTEXT_MODE)
 
 
-# def test_set_data_source():
-    
+def test_set_data_source(mock_get_context, mock_df, mock_config):
+    mock_context = mock_get_context.return_value
+    mock_context.data_sources.add_or_update_pandas = MagicMock()
+    expected_value = MagicMock()
+    mock_context.data_sources.add_or_update_pandas.return_value = expected_value
+
+    result = GreatExpectationsChecker(mock_df, mock_config.CONTEXT_MODE)
+
+    result.set_data_source(mock_config.DATA_SOURCE)
+
+    mock_context.data_sources.add_or_update_pandas.assert_called_once_with(
+        mock_config.DATA_SOURCE
+    )
+    assert result.data_source == expected_value
+
+
+def test_set_data_asset(mock_get_context, mock_df, mock_config):
+    mock_data_source = MagicMock()
+    mock_get_context.data_sources.add_or_update_pandas.return_value = mock_data_source
+
+    result = GreatExpectationsChecker(mock_df, mock_config.CONTEXT_MODE)
+
+    mock_data_source.add_dataframe_asset = MagicMock()
+    result.data_source = mock_data_source
+
+    result.set_data_asset(mock_config.DATA_ASSET)
+
+    mock_data_source.add_dataframe_asset.assert_called_once_with(
+        name=mock_config.DATA_ASSET
+    )
+    assert result.data_asset == mock_data_source.add_dataframe_asset.return_value
+
+
+def test_set_data_docs_site_try_branch(mock_get_context, mock_df, mock_config):
+    mock_context = mock_get_context.return_value
+    mock_context.add_data_docs_site = MagicMock()
+
+    result = GreatExpectationsChecker(mock_df, mock_config.CONTEXT_MODE)
+
+    result.set_data_docs_site(mock_config.SITE_NAME, mock_config.SITE_CONFIG)
+
+    mock_context.add_data_docs_site.assert_called_once_with(
+        site_name=mock_config.SITE_NAME, site_config=mock_config.SITE_CONFIG
+    )
+
+
+@pytest.mark.skip(reason="Currently working on this")
+def test_set_data_docs_site_except_branch(mock_get_context, mock_df, mock_config):
+    result = GreatExpectationsChecker(mock_df, mock_config.CONTEXT_MODE)
+
+    mock_get_context.add_data_docs_site = MagicMock(
+        side_effect=Exception("Test exception")
+    )
+    mock_get_context.update_data_docs_site = MagicMock()
+
+    result.set_data_docs_site(
+        site_name=mock_config.SITE_NAME, site_config=mock_config.SITE_CONFIG
+    )
+
+    mock_get_context.update_data_docs_site.assert_called_once_with(
+        site_name=mock_config.SITE_NAME, site_config=mock_config.SITE_CONFIG
+    )
+    mock_get_context.add_data_docs_site.assert_called_once_with(
+        site_name=mock_config.SITE_NAME, site_config=mock_config.SITE_CONFIG
+    )
