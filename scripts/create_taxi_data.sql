f7aa5f9fb5f90ec8945e5f6fb3eba7438f17f854
Matheus Brizolla <matheusfnx@hotmail.com>
2025-02-03 18:31:05 +0000

    feat: add table script creation

---
+++ scripts/create_taxi_data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE taxi_data (
+    vendor_id INT NOT NULL,
+    pickup_datetime TIMESTAMP NOT NULL,
+    dropoff_datetime TIMESTAMP NOT NULL,
+    passenger_count INT NOT NULL,
+    trip_distance REAL NOT NULL,
+    rate_code_id INT NOT NULL,
+    store_and_fwd_flag VARCHAR(10) NOT NULL,
+    pickup_location_id INT NOT NULL,
+    dropoff_location_id INT NOT NULL,
+    payment_type INT NOT NULL,
+    fare_amount REAL NOT NULL,	
+    extra REAL NOT NULL,
+    mta_tax REAL NOT NULL,
+    tip_amount REAL NOT NULL,
+    tolls_amount REAL NOT NULL,
+    improvement_surcharge REAL NOT NULL,
+    total_amount REAL NOT NULL,
+    congestion_surcharge REAL
+);
